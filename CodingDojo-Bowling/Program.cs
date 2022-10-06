1668dd25c7facabb808f18617772ceb3ec82d873
Jérémy Hure <hure.j@sfeir.com>
2022-10-06 16:57:10 +0200

    Program.cs

--- CodingDojo-Bowling/Program.cs
+++ CodingDojo-Bowling/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using CodingDojo_Bowling;
+
+Console.WriteLine("Ecrire un table de score : (X pour strike, / pour spare, - pour rien)");
+Console.WriteLine("Exemple : 6/ 4/ X -5 -- -/ 32 7- X 5/2");
+string? scores = Console.ReadLine();
+if (scores != null)
+{
+    BowlingGame game = new BowlingGame(scores);
+    Console.WriteLine("\nScore : " + game.Result);
+}
